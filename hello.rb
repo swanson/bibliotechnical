99e70f0d2eea117ee2a1605268e23e2352778745
Matt Swanson <swanson@purdue.edu>
2011-01-23 21:34:08 -0500

    adding mongodb backend and form

--- hello.rb
+++ hello.rb
@@ -1,6 +1,32 @@
 require 'rubygems'
 require 'sinatra'
+require 'uri'
+require 'mongo'
+require 'erb'
+require 'json'
+
+configure do
+  uri = URI.parse(ENV['MONGOHQ_URL'])
+  conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+  db = conn.db(uri.path.gsub(/^\//, ''))
+  $collection = db.collection("contacts")
+end
 
 get '/' do
-    "Hello there"
+  erb :index
+end
+
+post '/subscribe' do
+  contact = params[:contact]
+  contact_type = contact.start_with?("@") ||
+                 !contact.include?("@") ? "twitter" : "email"
+
+  doc = {
+    "contact" => contact,
+    "type"    => contact_type,
+    "referer" => params[:referer],
+  }
+   
+  $collection.insert(doc)
+  #doc.to_json
 end
